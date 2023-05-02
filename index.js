d4efeac1d63ec2c25a71dda6f6c6ae22ba5f6d5f
GersonADS <shiranuime@gmail.com>
2023-05-02 20:49:14 -0300

    js-2

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ let SoundChange = document.querySelector('#sound');
 
 let BackgroundVideos = [
     './assets/video/y2meta.com-Desktop Wallpaper Coven Ahri-(1080p60).mp4',
-    './assets/video/y2meta.com-Desktop Wallpaper Coven Ahri-(1080p60).mp4',
-    './assets/video/y2meta.com-Desktop Wallpaper Coven Ahri-(1080p60).mp4'
+    './assets/video/y2meta.com-(League of Legends) Blood Moon Yasuo _Animated_ Splash art-(1080p).mp4',
+    './assets/video/y2meta.com-Elementalist Lux video for deskscapes-(1080p).mp4'
 ]
 
 let BackgroundMusics = [
@@ -22,13 +22,18 @@ let index =0;
 ;$Arrow_left
 .addEventListener
 ('click',()=>{
-
+    if(index<= 0)index =3;
+    index--;
+    BackgroundChange.src = `${BackgroundVideos[index]}`
+    
+    console.log(`arrowleft `+index)
 });
 
 ;$Arrow_right
 .addEventListener
 ('click',()=>{
     index++;
-    if(index=== 3)index =0;
+    if(index>= 3)index =0;
     BackgroundChange.src = `${BackgroundVideos[index]}`
+    console.log(`arrowRight `+index)
 });
